f9b218c610b82bf3ddba8c20669aae7c154fed4a
S-MunSoo <godmsoo93@gmail.com>
2022-07-05 20:47:26 +0900

    리뷰 UI 성공

--- src/component/MovieReview.jsx
+++ src/component/MovieReview.jsx
@@ -9,8 +9,21 @@ const MovieReview = ({ review }) => {
         <div className="review-btn">
           <Button variant="danger">REVIEWS</Button>
         </div>
-        <div>
-          <p>{review.results && review.results[0].content}</p>
+        <div className="review-area">
+          <div className="review-content">
+            <p>
+              {review.results &&
+                review.results.map((item) => <p>{item.content}</p>)}
+            </p>
+          </div>
+
+          <div className="review-author">
+            <h4>
+              {review.results &&
+                review.results.map((item) => <h4>{item.author}</h4>)}
+            </h4>
+          </div>
+          {/* <span>수정 날짜</span> */}
         </div>
       </Container>
     </div>
--- src/component/WatchTrailer.jsx
+++ src/component/WatchTrailer.jsx
@@ -2,36 +2,44 @@ import React from "react";
 import { useState } from "react";
 import { Button, Modal } from "react-bootstrap";
 import YouTube from "react-youtube";
+import ModalBody from "react-bootstrap/ModalBody";
 
 const WatchTrailer = ({ Trailer }) => {
-  const [lgShow, setLgShow] = useState(false);
+  const [smShow, setSmShow] = useState(false);
   console.log("Trailer??", Trailer);
+
   const opts = {
-    height: "390",
-    width: "640",
+    height: "540px",
+    width: "960pxx",
     playerVars: {
       autoplay: 1,
+      rel: 0,
     },
   };
 
   return (
     <div>
-      <Button onClick={() => setLgShow(true)}>클릭!</Button>
-      <Modal
-        size="lg"
-        show={lgShow}
-        onHide={() => setLgShow(false)}
-        aria-labelledby="example-modal-sizes-title-lg"
-      >
-        <Modal.Body>
-          {
-            <YouTube
-              videoId={Trailer.results && Trailer.results[0].key}
-              opts={opts}
-            />
-          }
-        </Modal.Body>
-      </Modal>
+      <Button onClick={() => setSmShow(true)} className="me-2">
+        Small modal
+      </Button>
+      <div className="youtube-modal">
+        <Modal
+          className="modal-title"
+          size="sm"
+          show={smShow}
+          onHide={() => setSmShow(false)}
+          aria-labelledby="example-modal-sizes-title-sm"
+        >
+          <ModalBody className="modal-body">
+            {
+              <YouTube
+                videoId={Trailer.results && Trailer.results[0].key}
+                opts={opts}
+              />
+            }
+          </ModalBody>
+        </Modal>
+      </div>
     </div>
   );
 };
